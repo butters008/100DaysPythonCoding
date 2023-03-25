aef80ae1450f8d208ff239e552d8981777948600
Butters <k.butterfield08@gmail.com>
2023-03-25 16:02:10 -0500

    Completed Day 1 from Course

---
+++ Day_1_10/Day1.py
@@ -0,0 +1,30 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this")
+print("print('what to print')")
+
+print("Simple debugging")
+
+print("String 1 " + "(+) concats to String 2 -> \n <- line break")
+print("Hello there! " + input("please type what you want to be printed: "))
+# <- Comment
+
+# Prints the numbers of letters in your name
+print(len(input("What is your name? ")))
+
+# Simple problem - switch variable values
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+# End of Day 1 challenge
+pet_name = input("What is your pet name?\n")
+city_name = input("What city did you grow up in?\n")
+print("Your band name is " + pet_name + " " + city_name)
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# 100 Days of Programming in Python
+___
+
+### This is the code repo for the udemy course (https://www.udemy.com/course/100-days-of-code/)
