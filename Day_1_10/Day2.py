7a43a0c3c2e85da32fcedfbbce061c5e1780b88c
Butters <k.butterfield08@gmail.com>
2023-03-29 13:06:52 -0500

    Just did end of day project, maybe go back and add other stuff to learn

--- Day_1_10/Day2.py
+++ Day_1_10/Day2.py
@@ -1,1 +1,10 @@
-print("Test")
+print("This is tip calculator")
+bill = float(input("What was the bill? "))
+people = int(input("How many people"))
+tip = int(input("How much of a tip do you want to give? 10% 12% 15% "))
+
+tipResult = bill * (tip / 100)
+total = (bill + tipResult) / people
+
+print("Your bill is ")
+print(round(total, 2))
